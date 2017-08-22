faef0d3ee4800cc50bdb72024da1738fd99b9f10
sujeek <810480876@qq.com>
2017-08-22 10:51:39 +0800

    修改

--- LeetCode-master/algorithms/java/src/Title-14-LongestCommonPrefix/Solution.java
+++ LeetCode-master/algorithms/java/src/Title-14-LongestCommonPrefix/Solution.java
@@ -7,7 +7,8 @@ public class LongestCommonPrefix {
 	        while (strs[i].indexOf(prefix) != 0) {
 	            prefix = prefix.substring(0, prefix.length() - 1);
 	            if (prefix.isEmpty()) return "";
-	        }        
+	        }  
+	    
 	    return prefix;
 	}
 }
